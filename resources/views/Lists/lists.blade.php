7ae768c2f830c05ceefbdcf9207cd6364fa29cb7
parthlmb1 <parthlmb007@gmail.com>
2019-02-09 18:32:49 +0530

    Show All List

--- resources/views/Lists/lists.blade.php
+++ resources/views/Lists/lists.blade.php
@@ -14,12 +14,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+    <style>
+        html, body {
+            font-family: 'PT Sans', sans-serif;
+            background-color: #fafafa;
+        }
+    </style>
 </head>
 <body>
 
-    <div class="container">
+<div class="container">
+    <div class="col-md-12 to_do_lists" style="width:100%;">
 
     </div>
+</div>
 
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -29,7 +40,20 @@
             url: '{{url("lists/all")}}',
             method: "GET",
             success: function (data) {
-                console.log(data);
+                let listsCount = data.length;
+                for (let count = 0; count < listsCount; count += 1) {
+                    let htmlString =
+                        "<div class='col-sm-4' style='margin:20px; padding:20px; display:inline-block; height:250px; border-radius: 10px; box-shadow: 1px 1px 5px'>" +
+                        "<div class='col-sm-12' style='font-size:24px;color:#616161'>" +
+                        "<h4>" + data[count]["name"] + "</h4>" +
+                        "<p style='text-align:right;margin-top:-65px;padding-top:5px;'> <i class='material-icons'>add_circle_outline</i> </p>" +
+                        "</div>" +
+                        "<div>" +
+                        "<p style='font-size:18px;color:#757575'>" + data[count]["description"] + "</p>" +
+                        "</div>" +
+                        "</div>";
+                    $(".to_do_lists").append(htmlString);
+                }
             },
             error: function () {
                 console.log("Error");
