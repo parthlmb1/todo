a4a01a2394c2f7cb846a498e6b4dd5203c9ca6c3
parthlmb1 <parthlmb007@gmail.com>
2019-02-13 21:24:36 +0530

    Datatable Search Bar style updated

--- resources/views/Lists/lists.blade.php
+++ resources/views/Lists/lists.blade.php
@@ -20,30 +20,58 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
     <style>
         html, body {
-            font-family: 'PT Sans', sans-serif;
+            font-family: 'Ubuntu', sans-serif;
             background-color: #fafafa;
-            /*margin: 0;*/
-            /*padding: 0;*/
         }
 
         .to_do_lists {
             margin: 20px 20px;
+            border: 1px solid black;
+            padding: 10px;
+            border-radius: 5px;
+            /*width: 100%;*/
+        }
+
+        table thead tr{
+            -webkit-box-shadow: 0px 6px 18px -3px rgba(0,0,0,0.61);
+            -moz-box-shadow: 0px 6px 18px -3px rgba(0,0,0,0.61);
+            box-shadow: 0px 6px 18px -3px rgba(0,0,0,0.61);
         }
 
         table th {
             margin-top: 20px;
             padding: 10px 10px;
-            letter-spacing: 2px;
+            letter-spacing: 1px;
             background-color: #37474f;
             color: #fff;
         }
 
         table th:first-child{
-            border-radius: 5px 0 0 0;
+            border-radius: 10px 0 0 0;
         }
 
         table th:last-child{
             border-radius:0 10px 0 0;
+        }
+
+        .dataTables_wrapper .dataTables_filter input[type=search] {
+            font-family: Georgia, "Times New Roman", Times, serif;
+            background: rgba(255,255,255,.1);
+            border: none;
+            border-radius: 4px;
+            font-size: 15px;
+            margin: 0;
+            outline: 0;
+            padding: 10px;
+            width: 100%;
+            box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            background-color: #e8eeef;
+            color:#8a97a0;
+            -webkit-box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+            box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+            margin-bottom: 10px;
         }
 
     </style>
@@ -87,6 +115,10 @@
         $('#list_table').dataTable({
             ajax: '{{url("lists/all")}}',
             dataSrc: 'data',
+            language: {
+                search: "",
+                searchPlaceholder: "Search :"
+            },
             columns: [
                 {data : 'id'},
                 {data : 'name'},
@@ -96,6 +128,7 @@
             ]
         });
 
+        // Margin between table and table list length dropdown
         $("#list_table_length").css("margin-bottom", "20px")
     });
 </script>
